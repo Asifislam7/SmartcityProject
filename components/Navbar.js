4134a98ded8e17e4f3280795feef916f1b73665e
Asif <syedasif9072@@gmail.com>
2023-04-16 16:12:49 +0530

    First commit after structurin

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/components/navbar.module.css'
+const Navbar = () => {
+  return (
+  <>
+  {/* WRITE THE CODE OF THE NAVBAR HERE */}
+  </>
+  )
+}
+
+export default Navbar
